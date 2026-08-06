9cc4f7e4c9c4af0771007617943670fe0beffc9e
SAKURAI Tayra <b4151069@edu.kit.ac.jp>
2026-08-06 11:02:43 +0900

    Alpha test.

--- stockpilemanneo/src/App.jsx
+++ stockpilemanneo/src/App.jsx
@@ -1,15 +1,25 @@
 import './App.css';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import SignUp from './components/signup/SignUp';
+import SignIn from './components/signin/SignIn';
+import SessionCheck from './sessionman/SessionCheck';
 
 function App() {
 
   return (
-    <>
-      <main>
-        <SignUp />
-      </main>
-    </>
+    <BrowserRouter>
+      <SessionCheck autoRedirect={true}>
+        <main>
+          <Routes>
+            <Route path="/" element={<SignUp />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/auth/callback" element={<SessionCheck />} />
+          </Routes>
+        </main>
+      </SessionCheck>
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
