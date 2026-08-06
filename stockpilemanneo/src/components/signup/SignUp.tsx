b51ec5e90b34591a85d90ddc54109e2656feeb75
SAKURAI Tayra <b4151069@edu.kit.ac.jp>
2026-08-06 11:04:33 +0900

    Alpha test.

--- stockpilemanneo/src/components/signup/SignUp.tsx
+++ stockpilemanneo/src/components/signup/SignUp.tsx
@@ -21,7 +21,7 @@ const Card = styled(MuiCard)(({ theme }) => ({
   width: '100%',
   padding: theme.spacing(4),
   gap: theme.spacing(2),
-  margin: 'auto',
+  margin: 0,
   boxShadow:
     'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
   [theme.breakpoints.up('sm')]: {
