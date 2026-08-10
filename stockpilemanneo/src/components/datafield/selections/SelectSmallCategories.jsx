4ff7783a813e0e1770ee8fcf05904858affe9c51
SAKURAI Tayra <b4151069@edu.kit.ac.jp>
2026-08-10 16:24:12 +0900

    Modified small category selector.

--- stockpilemanneo/src/components/datafield/selections/SelectSmallCategories.jsx
+++ stockpilemanneo/src/components/datafield/selections/SelectSmallCategories.jsx
@@ -70,178 +70,103 @@ function SelectSmallCategories({ name, id, largeCategory, setLargeCategory }) {
     setLoading(false);
   };
 
-  if (!setLargeCategory) {
-    return (
-      <AutocompleteElement
-        name={name}
-        options={options}
-        loading={loading}
-        autocompleteProps={{
-          id,
-          value,
-          fullWidth: true,
-          getOptionLabel(option) {
-            return option.id ? option.name : `${option.name}を追加`;
-          },
-          groupBy(option) {
-            return option.large_categories.name;
-          },
-          selectOnFocus: true,
-          clearOnBlur: true,
-          handleHomeEndKeys: true,
-          resetHighlightOnMouseLeave: true,
-          async onOpen() {
-            if (!options) await loadOptions();
-          },
-          filterOptions(options, params) {
-            const filtered = filter(options, params);
-
-            if (largeCategory?.id && params.inputValue && !filtered.some(val => val.name == params.inputValue)) {
-              filtered.push({
-                name: params.inputValue,
-                large_categories: {
-                  name: largeCategory.name,
-                  id: largeCategory.id,
-                },
-              });
-            }
-
-            return filtered;
-          },
-          async onChange(event, newValue) {
-            if (largeCategory?.id) {
-              if (newValue && typeof newValue === 'string') {
-                const { data, error } = await supabase
-                  .from('small_categories')
-                  .insert({
-                    name: newValue,
-                    large_category_id: largeCategory.id,
-                    vector: await createEmbeddingVector(newValue)
-                  })
-                  .select('name, id, large_categories(id, name)');
-
-                await loadOptions();
-
-                if (error) throw error;
-                if (data) setValue(data[0]);
-              } else if (newValue?.id) {
-                setValue(newValue);
-              } else if (newValue?.large_categories?.id) {
-                const { data, error } = await supabase
-                  .from('small_categories')
-                  .insert({
-                    name: newValue.name,
-                    large_category_id: newValue.large_categories.id,
-                    vector: await createEmbeddingVector(newValue.name),
-                  })
-                  .select('id, name, large_categories(id, name)');
-
-                await loadOptions();
-
-                if (error) throw error;
-                if (data) setValue(data[0]);
-              } else {
-                setValue(newValue);
-              }
-            } else {
-              setValue(newValue);
-            }
-          },
-        }}
-      />
-    );
-  }
-
-
   return (
     <AutocompleteElement
       name={name}
       options={options}
       loading={loading}
       autocompleteProps={{
-        fullWidth: true,
-        selectOnFocus: true,
-        clearOnBlur: true,
-        handleHomeEndKeys: true,
-        resetHighlightOnMouseLeave: true,
-        getOptionLabel(option) {
-          return option.id ? option.name : `${option.name}を追加する`;
-        },
-        groupBy(option) {
-          return option.large_categories.name;
-        },
+        id,
+        value,
         async onOpen() {
-          if (!options) await loadOptions();
+          if (options)
+            return;
+
+          await loadOptions();
+        },
+        async onChange(event, newValue) {
+          if (typeof newValue === 'string') {
+            if (largeCategory?.id) {
+              const { data, error } = await supabase
+                .from('small_categories')
+                .insert({
+                  large_category_id: largeCategory.id,
+                  name: newValue,
+                  vector: await createEmbeddingVector(newValue),
+                })
+                .select('id, name, large_categories(id, name)');
+
+              if (error) throw error;
+              if (data) setValue(data[0]);
+            } else {
+              throw new Error('The value is not valid.');
+            }
+          } else if (newValue?.id) {
+            setValue(newValue);
+
+            if (newValue.large_categories.id && setLargeCategory)
+              setLargeCategory(newValue.large_categories);
+          } else if (newValue && largeCategory?.id) {
+            const { data, error } = await supabase
+              .from('small_categories')
+              .insert({
+                name: newValue.name,
+                large_category_id: largeCategory.id,
+                vector: await createEmbeddingVector(newValue.name),
+              })
+              .select('id, name, large_categories(id, name)');
+
+            if (error) throw error;
+            if (data) setValue(data[0]);
+          }
+
+          await loadOptions();
+        },
+        getOptionLabel(option) {
+          if (typeof option === 'string')
+            return option;
+          return option.name;
         },
         filterOptions(options, params) {
           const filtered = filter(options, params);
 
           const { inputValue } = params;
 
-          const exists = filtered.some(val => val.name == inputValue);
+          const isExisting = filtered.some(value => value.name == inputValue);
 
-          if (!exists && largeCategory?.id) {
+          if (inputValue && !isExisting && largeCategory?.id)
             filtered.push({
               name: inputValue,
-              large_categories: {
-                name: largeCategory.name,
-                id: largeCategory.id,
-              },
+              large_categories: largeCategory,
             });
-          }
+
+          if (largeCategory?.id && !setLargeCategory)
+            return filtered.filter(value => value.large_categories.id == largeCategory.id);
 
           return filtered;
         },
-        async onChange(event, newValue) {
-          if (newValue?.id && newValue?.large_categories?.id) {
-            setValue(newValue);
-            setLargeCategory({
-              name: newValue.large_categories.name,
-              id: newValue.large_categories.id,
-            });
-          } else if (!newValue) {
-            setValue(newValue);
-          } else if (newValue.large_categories.id) {
-            const { data, error } = await supabase
-              .from('small_categories')
-              .insert({
-                name: newValue.name,
-                vector: await createEmbeddingVector(newValue.name),
-                large_category_id: newValue.large_categories.id,
-              })
-              .select('id, name, large_categories(id, name)');
+        renderOption(props, option) {
+          const { key, ...otherProps } = props;
 
-            await loadOptions();
-
-            if (error) throw error;
-            if (data) setValue(data[0]);
-          } else {
-            const { data: d1, error: e1 } = await supabase
-              .from('large_categories')
-              .insert({
-                name: newValue.large_categories.name,
-                vector: await createEmbeddingVector(newValue.large_categories.name),
-              })
-              .select('id, name');
-
-            if (e1) throw e1;
-            if (d1) setLargeCategory(d1[0]);
-
-            const { data: d2, error: e2 } = await supabase
-              .from('small_categories')
-              .insert({
-                name: newValue.name,
-                vector: await createEmbeddingVector(newValue.name),
-                large_category_id: d1[0].id,
-              })
-              .select('id, name, large_categories(id, name)');
-
-            await loadOptions();
-
-            if (e2) throw e2;
-            if (d2) setValue(d2[0]);
-          }
+          return (
+            <li key={key} {...otherProps}>
+              {option.id ? option.name : `${option.name}を追加する`}
+            </li>
+          );
         },
+        groupBy(option) {
+          return option.large_categories.name;
+        },
+        getOptionKey(option) {
+          if (typeof option === 'string')
+            return option;
+
+          return option.name;
+        },
+        freeSolo: true,
+        handleHomeEndKeys: true,
+        selectOnFocus: true,
+        clearOnBlur: true,
       }}
     />
   );
