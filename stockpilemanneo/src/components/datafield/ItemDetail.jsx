06e99f1f273bb292e55b5c15d82df5328fbf426c
SAKURAI Tayra <b4151069@edu.kit.ac.jp>
2026-08-16 10:58:05 +0900

    Modified the error handling.

--- stockpilemanneo/src/components/datafield/ItemDetail.jsx
+++ stockpilemanneo/src/components/datafield/ItemDetail.jsx
@@ -211,6 +211,7 @@ function ItemDetail({ id }) {
                     }
                   }
                 }}
+                onError={message => setErr(message)}
               />
             </Container>
             <Button
