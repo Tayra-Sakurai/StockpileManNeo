9b5fca6b768ad5f0a327787e9d41c6809f1156b0
SAKURAI Tayra <b4151069@edu.kit.ac.jp>
2026-08-20 18:57:53 +0900

    Small improvements.

--- stockpilemanneo/src/App.jsx
+++ stockpilemanneo/src/App.jsx
@@ -29,41 +29,39 @@ import AIChat from './components/chat/AIChat.jsx';
 function App() {
   return (
     <BrowserRouter>
-      <AppTheme>
-        <Routes>
-          <Route path="/" element={<AppBaseElement />}>
-            <Route path="/" element={<AIChat />} />
-            <Route path="Search" element={<SearchPage />} />
-            <Route path="Edit" element={<EditBlock />}>
-              <Route path="large_categories/:id" element={<LargeCategoryEdit />} />
-              <Route path="small_categories/:id" element={<SmallCategoryEdit />} />
-              <Route path="locations/:id" element={<LocationEdit />} />
-              <Route path="items/:id" element={<ItemEdit />} />
-              <Route path="large_large_categories/:id" element={<LargeLargeCategoryEdit />} />
-            </Route>
-            <Route path="Add" element={<AddBlock />}>
-              <Route path="large_categories" element={<LargeCategoryDetail />} />
-              <Route path="small_categories" element={<SmallCategoryDetail />} />
-              <Route path="locations" element={<LocationDetail />} />
-              <Route path="items" element={<ItemDetail />} />
-              <Route path="large_large_categories" element={<LargeLargeCategoryDetail />} />
-            </Route>
-            <Route path="View/large_large_categories" element={<LargeLargeCategoriesView />} />
-            <Route path="View/large_categories/large_large_categories/:code" element={<LargeCategoriesView />} />
-            <Route path="View/large_categories" element={<LargeCategoriesView />} />
-            <Route path="View/locations" element={<LocationsView />} />
-            <Route path="View/items/:table/:code" element={<ItemsView />} />
-            <Route path="View/items" element={<ItemsView />} />
-            <Route path="View/small_categories" element={<SmallCategoriesView />} />
-            <Route path="View/small_categories/large_categories/:code" element={<SmallCategoriesView />} />
-            <Route path="Chat" element={<AIChat />} />
+      <Routes>
+        <Route path="/" element={<AppBaseElement />}>
+          <Route path="/" element={<AIChat />} />
+          <Route path="Search" element={<SearchPage />} />
+          <Route path="Edit" element={<EditBlock />}>
+            <Route path="large_categories/:id" element={<LargeCategoryEdit />} />
+            <Route path="small_categories/:id" element={<SmallCategoryEdit />} />
+            <Route path="locations/:id" element={<LocationEdit />} />
+            <Route path="items/:id" element={<ItemEdit />} />
+            <Route path="large_large_categories/:id" element={<LargeLargeCategoryEdit />} />
           </Route>
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/auth/callback" element={<SessionCheck />} />
-          <Route path="/reset-password" element={<PasswordReset />} />
-        </Routes>
-      </AppTheme>
+          <Route path="Add" element={<AddBlock />}>
+            <Route path="large_categories" element={<LargeCategoryDetail />} />
+            <Route path="small_categories" element={<SmallCategoryDetail />} />
+            <Route path="locations" element={<LocationDetail />} />
+            <Route path="items" element={<ItemDetail />} />
+            <Route path="large_large_categories" element={<LargeLargeCategoryDetail />} />
+          </Route>
+          <Route path="View/large_large_categories" element={<LargeLargeCategoriesView />} />
+          <Route path="View/large_categories/large_large_categories/:code" element={<LargeCategoriesView />} />
+          <Route path="View/large_categories" element={<LargeCategoriesView />} />
+          <Route path="View/locations" element={<LocationsView />} />
+          <Route path="View/items/:table/:code" element={<ItemsView />} />
+          <Route path="View/items" element={<ItemsView />} />
+          <Route path="View/small_categories" element={<SmallCategoriesView />} />
+          <Route path="View/small_categories/large_categories/:code" element={<SmallCategoriesView />} />
+          <Route path="Chat" element={<AIChat />} />
+        </Route>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/auth/callback" element={<SessionCheck />} />
+        <Route path="/reset-password" element={<PasswordReset />} />
+      </Routes>
     </BrowserRouter>
   );
 }
