d931f2cf116eb017afc54997bda5be259a8b08ae
SAKURAI Tayra <b4151069@edu.kit.ac.jp>
2026-08-23 14:59:56 +0900

    Added license notices.

--- stockpilemanneo/src/components/datafield/SmallCategoryDetail.jsx
+++ stockpilemanneo/src/components/datafield/SmallCategoryDetail.jsx
@@ -1,4 +1,18 @@
 /**
+ * @fileoverview The common component to edit and add a small category.
+ * @author Tayra Sakurai <tayra_sakurai@icloud.com>
+ * @copyright Copyright (C) 2026 Tayra Sakurai <tayra_sakurai@icloud.com>
+ * @license Copyright (C) 2026 Tayra Sakurai
+ * 
+ * This is a part of StockpileMan Neo.
+ * 
+ * StockpileMan Neo is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * StockpileMan Neo is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with StockpileMan Neo. If not, see https://www.gnu.org/licenses/.
+ */
+/**
  * @typedef {object} SmallCategoryFormData The small category's form data object.
  * @property {?import("./selections/SelectLargeLargeCategories.jsx").LargeLargeCategoryCandidate} large_large_categories The largest category.
  * @property {?import("./selections/SelectLargeCategories.jsx").LargeCategoryCandidate} large_categories The large category related to the small category.
--- stockpilemanneo/src/types/supabase.ts
+++ stockpilemanneo/src/types/supabase.ts
@@ -1,3 +1,17 @@
+/**
+ * @fileoverview The Supabase type exporter.
+ * @author Tayra Sakurai <tayra_sakurai@icloud.com>
+ * @copyright Copyright (C) 2026 Tayra Sakurai <tayra_sakurai@icloud.com>
+ * @license Copyright (C) 2026 Tayra Sakurai
+ * 
+ * This is a part of StockpileMan Neo.
+ * 
+ * StockpileMan Neo is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * StockpileMan Neo is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with StockpileMan Neo. If not, see https://www.gnu.org/licenses/.
+ */
 export type Json =
   | string
   | number
