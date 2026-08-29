e36e95b229b472241f22df5c9504f8046dff5e3f
SAKURAI Tayra <b4151069@edu.kit.ac.jp>
2026-08-29 12:23:20 +0900

    Bugfix.

--- stockpilemanneo/vite.config.js
+++ stockpilemanneo/vite.config.js
@@ -8,9 +8,18 @@ export default defineConfig({
   plugins: [
     plugin(),
     license({
+      sourcemap: true,
+      banner: {
+        commentStyle: 'regular',
+        content: {
+          file: path.join(__dirname, 'LICENSE.txt'),
+          encoding: 'utf-8',
+        },
+      },
       thirdParty: {
         includePrivate: true,
         includeSelf: true,
+        multipleVersions: true,
         output: {
           file: path.join(__dirname, 'dist', 'dependencies.txt'),
           encoding: 'utf-8',
