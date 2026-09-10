c8f45f0b0685d83eac9bcb30b82219cb1f337c52
SAKURAI Tayra <b4151069@edu.kit.ac.jp>
2026-09-10 12:43:24 +0900

    Added a sorter.

--- stockpilemanneo/src/components/views/Views/LargeLargeCategories/LargeLargeCategoriesTable.jsx
+++ stockpilemanneo/src/components/views/Views/LargeLargeCategories/LargeLargeCategoriesTable.jsx
@@ -1,5 +1,6 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TableSortLabel } from "@mui/material";
 import LargeLargeCategoryRow from "./LargeLargeCategoryRow.jsx";
+import { useState } from "react";
 
 /**
  * Largest category data type.
@@ -18,12 +19,90 @@ import LargeLargeCategoryRow from "./LargeLargeCategoryRow.jsx";
  */
 
 /**
+ * Gets the number of items
+ * @param {Object} param The parameter object.
+ * @param {import("./LargeLargeCategoryRow.jsx").LLDetail} param.large_categories The large category data.
+ * @returns {number}
+ */
+function getNumberOfItems({ large_categories }) {
+  /**
+   * The number of items
+   * @type {number}
+   */
+  let n = 0;
+
+  for (const { small_categories } of large_categories) {
+    for (const { items: [{ count }] } of small_categories) {
+      n += count;
+    }
+  }
+
+  return n;
+}
+
+/**
+ * Gets the sorter by the data.
+ * @param {keyof LargeLargeCategoryData} orderBy The standard of the sorting.
+ * @param { "desc" | "asc" } direction The order direction.
+ * @returns {LargeLargeCategorySorter}
+ */
+function getSorter(orderBy, direction) {
+  switch (orderBy) {
+    case 'id':
+      return (direction === 'asc') ?
+        (a, b) => a.id - b.id :
+        (a, b) => b.id - a.id;
+
+    case 'name':
+      return (direction === 'asc') ?
+        (a, b) => a.name.localeCompare(b.name) :
+        (a, b) => b.name.localeCompare(a.name);
+
+    case 'large_categories':
+    default:
+      return (direction === 'asc') ?
+        (a, b) => getNumberOfItems(a) - getNumberOfItems(b) :
+        (a, b) => getNumberOfItems(b) - getNumberOfItems(a);
+  }
+}
+
+/**
  * Largest categories display table.
  * @param {object} props The props.
  * @param {LargeLargeCategoryData[]} props.displayData The displaying data.
  * @returns
  */
 function LargeLargeCategoriesTable({ displayData }) {
+  const [orderBy, setOrderBy] =
+    /**
+     * @type {<S = keyof LargeLargeCategoryData>(initialState: S) => [
+     *   keyof LargeLargeCategoryData,
+     *   import("react").Dispatch.<import("react").SetStateAction.<keyof LargeLargeCategoryData>>
+     * ]}
+     */
+    (useState)('large_categories');
+
+  const [order, setOrder] =
+    /**
+     * @type {
+     *   <S = "asc" | "desc">(initialState: S) => [
+     *     "asc" | "desc",
+     *     import("react").Dispatch.<import("react").SetStateAction.<"asc" | "desc">>
+     * ]}
+     */
+    (useState)('asc');
+
+  /**
+   * Handle the click action.
+   * @param {keyof LargeLargeCategoryData} key The key.
+   * @returns {() => void}
+   */
+  const handleClick = key => () => {
+    const isAsc = (key == orderBy) && (order === 'asc');
+    setOrderBy(key);
+    setOrder(isAsc ? 'desc' : 'asc');
+  };
+
   return (
     <TableContainer
       sx={{ width: '100%', overflow: 'scroll' }}
@@ -31,14 +110,38 @@ function LargeLargeCategoriesTable({ displayData }) {
       <Table sx={{ width: 'max-content' }}>
         <TableHead>
           <TableRow>
-            <TableCell>番号</TableCell>
-            <TableCell>分類名</TableCell>
-            <TableCell align="right">品目数</TableCell>
+            <TableCell>
+              <TableSortLabel
+                active={orderBy === 'id'}
+                direction={(orderBy === 'id') ? order : 'asc'}
+                onClick={handleClick('id')}
+              >
+                番号
+              </TableSortLabel>
+            </TableCell>
+            <TableCell>
+              <TableSortLabel
+                active={orderBy === 'name'}
+                direction={(orderBy === 'name') ? order : 'asc'}
+                onClick={handleClick('name')}
+              >
+                分類名
+              </TableSortLabel>
+            </TableCell>
+            <TableCell align="right">
+              <TableSortLabel
+                active={orderBy === 'large_categories'}
+                direction={(orderBy === 'large_categories') ? order : 'asc'}
+                onClick={handleClick('large_categories')}
+              >
+                品目数
+              </TableSortLabel>
+            </TableCell>
             <TableCell>操作</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {displayData.map(({ id, ...others }) => <LargeLargeCategoryRow itemId={id} {...others} />)}
+          {displayData.toSorted(getSorter(orderBy, order)).map(({ id, ...others }) => <LargeLargeCategoryRow itemId={id} {...others} />)}
         </TableBody>
       </Table>
     </TableContainer>
